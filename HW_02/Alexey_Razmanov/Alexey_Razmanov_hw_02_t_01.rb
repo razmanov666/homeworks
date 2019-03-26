8f14f02564979890ab439040566f8a6e75e07ca1
Алексей Разманов <kurama@pop-os.localdomain>
2019-03-26 21:58:28 +0300

    edit files

--- HW_02/Alexey_Razmanov/Alexey_Razmanov_hw_02_t_01.rb
+++ HW_02/Alexey_Razmanov/Alexey_Razmanov_hw_02_t_01.rb
@@ -1,2 +1,2 @@
-degree = "0 + PI";
-puts "Degrees between arrows on clock in moment when time 3:15 = #{degree}"
+degree = "0 + PI*n";
+puts "Degrees between arrows on clock in moment when time 3:15 = #{degree}, n is integer"
