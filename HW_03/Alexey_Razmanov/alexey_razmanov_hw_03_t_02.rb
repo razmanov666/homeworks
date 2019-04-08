6ca27a63cf0fabebefe2259e1e05c9afd13b6e2f
Алексей Разманов <kurama@pop-os.localdomain>
2019-04-08 15:32:32 +0300

    Fixed task 2,3 (temp var is deleted)

--- HW_03/Alexey_Razmanov/alexey_razmanov_hw_03_t_02.rb
+++ HW_03/Alexey_Razmanov/alexey_razmanov_hw_03_t_02.rb
@@ -1,12 +1,28 @@
-def task_2(string)
-  res = []
-  string.each_line do |s|
-    ip = s[/.* - -/]
-    date = s[/[\[].*[\]]/]
-    path = s[/T .* H/]
+def task_2(str)
+  
+  task_2_sort(str).map do |line|
+    ip = line[/.* - -/]
+    date = line[/[\[].*[\]]/]
+    path = line[/T .* H/]
     if date && path && ip
-      res << date[1..-2] + ' FROM: ' + ip[0..-5] + ' TO:' + path[1..-3].upcase
+      date[1..-2] + ' FROM: ' + ip[0..-5] + ' TO:' + path[1..-3].upcase
     end
   end
-  res
 end
+
+def task_2_sort(str)
+  str.each_line.select do |line|
+    ip = line[/.* - -/]
+    date = line[/[\[].*[\]]/]
+    path = line[/T .* H/]
+    if date && path && ip
+     date[1..-2] + ' FROM: ' + ip[0..-5] + ' TO:' + path[1..-3].upcase
+    end
+  end
+end
+
+str = "10.6.246.103 - - [23/Apr/2018:20:30:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0498
+10.6.246.101 - - [23/Apr/2018:20:30:42 +0300] \"POST /test/2/run HTTP/1.1\" 200 - 0.2277
+2018-04-23 20:30:42: SSL ERROR, peer: 10.6.246.101, peer cert: , #<Puma::MiniSSL::SSL: System error: Undefined error: 0 - 0>
+10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] \"POST /test/2/messages HTTP/1.1\" 200 48 0.0290"
+puts task_2(str)
--- HW_03/Alexey_Razmanov/alexey_razmanov_hw_03_t_03.rb
+++ HW_03/Alexey_Razmanov/alexey_razmanov_hw_03_t_03.rb
@@ -1,19 +1,16 @@
 require 'date'
 
 def task_3(string)
-  res = '0'
-  arr = []
-  string.each_line do |s|
-    arr.push(s) if s.include?('Calling core with action:')
-  end
-  res = time_btwn_acts(arr) if arr.size >= 2
-  res
+  string = (string.each_line.select do |line|
+    line.include?('Calling core with action:')
+  end)
+  string.size >= 2 ? time_btwn_acts(string) : '0'
 end
 
-def time_btwn_acts(arr)
-  finish = DateTime.strptime(arr.last,
+def time_btwn_acts(string)
+  finish = DateTime.strptime(string.last,
                              '%Y-%m-%d %H:%M:%S.%L').strftime('%Q').to_i
-  start = DateTime.strptime(arr.first,
+  start = DateTime.strptime(string.first,
                             '%Y-%m-%d %H:%M:%S.%L').strftime('%Q').to_i
   (finish - start) / 1000.0
 end
